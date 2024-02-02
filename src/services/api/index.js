2da5c9e375af79a2cf3515354ae43320e94a2afa
Gyomar Pacheco <gyomar.eduardo.pacheco@gmail.com>
2024-02-01 23:02:33 -0500

    cambiamos la configuracion del api

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,26 +1,6 @@
-const API = import.meta.env.VITE_API_URL;
+const isDevelopment = import.meta.env.MODE === 'development';
+const API = isDevelopment ? 'http://localhost:3000' : import.meta.env.VITE_API_URL;
 const apiKey = import.meta.env.VITE_API_KEY;
-
-const createEndpoint = (basePath, routes) => {
-  const createUrl = (path, params) => {
-    const notSlash = typeof params === 'string' && (params.startsWith('?') || params === '');
-    const separator = notSlash ? '' : '/';
-
-    return `${path}${separator}${params}`;
-  };
-
-  const result = {};
-
-  for (const [key, value] of Object.entries(routes)) {
-    if (typeof value === 'function') {
-      result[key] = (...args) => createUrl(basePath, value(...args));
-    } else {
-      result[key] = createUrl(basePath, value);
-    }
-  }
-
-  return result;
-};
 
 const endPoints = {
   apiKey,
